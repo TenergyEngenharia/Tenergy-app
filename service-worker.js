9f25221ad727e29e5c439c7a234e957199f51ad6
TenergyEngenharia <estagiariosengenharia1@gmail.com>
2026-06-26 11:15:04 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'tenergy-rdo-va1';
+
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'logo-preto.png',
+  'logo-branca.png',
+  'logo-simbolo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
